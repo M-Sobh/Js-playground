071518f5be2985a9eabd6d6ebf8b23100f1e7e08
Mohamed Sobh <eng.m_sobhe@yahoo.com>
2021-02-15 18:39:07 +0000

    methods

--- abc.js
+++ abc.js
@@ -1,14 +1,14 @@
 //capitalize first letter of each word
 
-// function capitaliz(str) {
-//   const strArr = str.toLowerCase().split(" ");
-//   for (let i = 0; i < strArr.length; i++) {
-//     strArr[i] =
-//       strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
-//   }
-//   return strArr.join(" ");
-// }
-// console.log(capitaliz("ah med is awsem"));
+function capitaliz(str) {
+  const strArr = str.toLowerCase().split(" ");
+  for (let i = 0; i < strArr.length; i++) {
+    strArr[i] =
+      strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+  }
+  return strArr.join(" ");
+}
+console.log(capitaliz("ah med is awsem"));
 
 ///////////////////////////////
 
